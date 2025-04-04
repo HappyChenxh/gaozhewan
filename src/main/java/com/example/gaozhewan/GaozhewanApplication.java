37a0b42a2b5623ca4afbf51bada8085b24040620
chenxh <952657115@qq.com>
2025-04-05 00:52:46 +0800

    1

---
+++ src/main/java/com/example/gaozhewan/GaozhewanApplication.java
@@ -0,0 +1,12 @@
+package com.example.gaozhewan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaozhewanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaozhewanApplication.class, args);
+    }
+}
